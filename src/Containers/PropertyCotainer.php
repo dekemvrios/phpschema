00aa21453306775470f62b5524ab7d8397667b9b
Rafael Becker <realfabecker@gmail.com>
2017-08-02 15:02:57 -0300

    [UPD] atualizado metodo make de propertyContainer

--- src/Containers/PropertyCotainer.php
+++ src/Containers/PropertyCotainer.php
@@ -4,6 +4,8 @@ namespace Solis\Expressive\Schema\Containers;
 
 use Solis\Expressive\Schema\Contracts\Entries\Property\ContainerContract as PropertyContainerContract;
 use Solis\Expressive\Schema\Contracts\Entries\Property\PropertyContract;
+use Solis\Expressive\Schema\Entries\Property;
+use Solis\Breaker\TException;
 
 /**
  * Class PropertyCotainer
@@ -16,6 +18,40 @@ class PropertyCotainer implements PropertyContainerContract
      * @var PropertyContract[]
      */
     private $properties;
+
+    /**
+     * make
+     *
+     * @param array $schema
+     *
+     * @return static
+     * @throws TException
+     */
+    public static function make($schema)
+    {
+        if (!array_key_exists(
+            'properties',
+            $schema
+        )
+        ) {
+            throw new TException(
+                __CLASS__,
+                __METHOD__,
+                "'properties' field has not been found for defining schema entry",
+                500
+            );
+        }
+        $propertiesEntry = $schema['properties'];
+
+        $instance = new self();
+        foreach ($propertiesEntry as $item) {
+            $instance->append(
+                Property::make($item)
+            );
+        }
+
+        return $instance;
+    }
 
     /**
      * setProperties
@@ -39,5 +75,20 @@ class PropertyCotainer implements PropertyContainerContract
     public function getProperties()
     {
         return $this->properties;
+    }
+
+    /**
+     * append
+     *
+     * Adiciona uma nova entrada a relação de propriedades
+     *
+     * @param PropertyContract $property
+     */
+    public function append($property)
+    {
+        array_push(
+            $this->properties,
+            $property
+        );
     }
 }
